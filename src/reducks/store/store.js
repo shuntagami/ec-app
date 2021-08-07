c572e4a9721b10a9408a3b23758f97edf7a6d3cd
shuntagami <shun.tagami@i-nations.tokyo>
2021-08-07 23:29:37 +0900

    Intro react-router and connected-react-router

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,18 +1,24 @@
+import { connectRouter, routerMiddleware } from "connected-react-router";
 import {
   createStore as reduxCreateStore,
   combineReducers,
+  applyMiddleware
 } from "redux";
 
 //import reducers
 import { UsersReducer } from "../users/reducers";
 
-export default function createStore() {
+export default function createStore(history) {
   return reduxCreateStore(
     // 1.分割したReducersをまとめる
     // 2.stateのカテゴリ別
     // 3.オブジェクトをreturnする(stateのデータ構造)
     combineReducers({
+      router: connectRouter(history),
       users: UsersReducer,
     }),
+    applyMiddleware(
+      routerMiddleware(history)
+    )
   );
 }
