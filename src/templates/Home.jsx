66788932bb86eaf42986d4ddf838c0011e0eb594
shuntagami <shun.tagami@i-nations.tokyo>
2021-08-08 00:05:35 +0900

    Add selectors.js

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getUserId } from "../reducks/users/selectors";
+import { getUserName } from "../reducks/users/selectors";
 
 const Home = () => {
+  const selector = useSelector(state => state);
+  const uid = getUserId(selector)
+  const userName = getUserName(selector)
   return (
-    <h2>Home</h2>
+    <div>
+      <h2>Home</h2>
+      <p>{ uid }</p>
+      <p>{ userName }</p>
+    </div>
   )
 }
 export default Home;
