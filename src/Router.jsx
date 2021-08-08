ffa2cdd1c8bceb3bf3c0e4c73c1ba6449d988f55
shuntagami <shun.tagami@i-nations.tokyo>
2021-08-08 15:17:03 +0900

    Remove unused file

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import { Login, Home } from "./templates";
-import { LoginContainer } from "./containers";
 
 const Router = () => {
   return (
     <Switch>
-      <Route exact path={"/login"} component={LoginContainer} />
-      {/* <Route exact path={"/login"} component={Login} */}
+      <Route exact path={"/login"} component={Login} />
       <Route exact path={"(/)?"} component={Home} />
     </Switch>
   );
