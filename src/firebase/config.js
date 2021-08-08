cf330425b662fddb9c48bb739406f14ca14dc4e7
shuntagami <shun.tagami@i-nations.tokyo>
2021-08-08 15:17:28 +0900

    Add firebaseConfig

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAu9V9d0ZzxqWdYMW4mL0kZ9BrOK-nxnPM",
+  authDomain: "ec-app-76ce9.firebaseapp.com",
+  projectId: "ec-app-76ce9",
+  storageBucket: "ec-app-76ce9.appspot.com",
+  messagingSenderId: "621312727011",
+  appId: "1:621312727011:web:270ae7f5fc7238f86832d6",
+  measurementId: "G-17Y81TLG9W"
+};
