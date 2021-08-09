eb790c581ba024cf037f4f1a9a4ce065b23bbb89
shuntagami <shun.tagami@i-nations.tokyo>
2021-08-09 21:04:32 +0900

    Implement delete and edit products

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -7,40 +7,56 @@ import Typography from '@material-ui/core/Typography';
 import NoImage from '../../assets/img/src/no_image.png';
 import {push} from 'connected-react-router'
 import { useDispatch } from 'react-redux';
+import { IconButton } from '@material-ui/core';
+import { Menu } from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import { useState } from 'react';
+import { deleteProduct } from '../../reducks/products/operations';
 
-// themaファイルのmaincolorやbreakpointを使える
-const useStyles = makeStyles((thema) => ({
+// themeファイルのmaincolorやbreakpointを使える
+const useStyles = makeStyles((theme) => ({
   root: {
-    [thema.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       margin: 8,
-      width: 'calc(50% - 16px)'
+      width: "calc(50% - 16px)",
     },
-    [thema.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("md")]: {
       margin: 16,
-      width: 'calc(33.3333% - 32px)'
-    }
+      width: "calc(33.3333% - 32px)",
+    },
   },
   content: {
-    display: 'flex',
-    padding: '16px 8px',
-    textAlign: 'left',
-    '&:last-child': {
-      paddingBottom: 16
-    }
+    display: "flex",
+    padding: "16 8",
+    textAlign: "left",
+    "&:last-child": {
+      paddingBottom: 16,
+    },
   },
   media: {
     height: 0,
-    paddingTop: '100%'
+    paddingTop: "100%",
   },
   price: {
-    color: thema.palette.secondary.main,
-    fontSize: 16
-  }
-}))
+    color: theme.palette.secondary.main,
+    fontSize: 16,
+  },
+}));
 
 const ProductCard = (props) => {
   const classes = useStyles();
   const dispatch = useDispatch();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
 
   const images = (props.images.length > 0 ) ? props.images[0].path : NoImage
   const price = props.price.toLocaleString();
@@ -62,6 +78,32 @@ const ProductCard = (props) => {
             ¥{price}
           </Typography>
         </div>
+        <IconButton onClick={handleClick}>
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem
+            onClick={() => {
+              dispatch(push(`/product/edit/${props.id}`))
+              handleClose()
+            }}
+          >
+            編集する
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              dispatch(deleteProduct(props.id))
+              handleClose()
+            }}
+          >
+            削除する
+          </MenuItem>
+        </Menu>
       </CardContent>
     </Card>
   )
